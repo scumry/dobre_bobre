61fd244d7e20033184d7b1ce0db0de9745f11bf8
scumry <diana.ma1310@mail.ru>
2023-11-08 19:36:00 +0500

    bobre

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        string image = "image/bobre.jpg";
+        string myString = image.ToString();
+        byte[] imageByt = File.ReadAllBytes(image);
+        string outputImage = "image/bobre.jpg";
+        File.WriteAllBytes(outputImage, imageByt);
+        Console.WriteLine("Изображение успешно сохранено по пути: " + outputImage);
+    }
+} 
